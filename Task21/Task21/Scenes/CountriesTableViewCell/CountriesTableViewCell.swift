78f8b34d9acaabf9d4e6bf0ccb6b117e5bce4b98
ananamgaladze <ananamgaladze3@gmail.com>
2024-04-26 08:44:20 +0400

    21

--- Task21/Task21/Scenes/CountriesTableViewCell/CountriesTableViewCell.swift
+++ Task21/Task21/Scenes/CountriesTableViewCell/CountriesTableViewCell.swift
@@ -5,94 +5,11 @@
 //  Created by ana namgaladze on 25.04.24.
 //
 
-//import UIKit
-//
-//class CountriesTableViewCell: UITableViewCell {
-//    static let identifier = "CountriesTableViewCell"
-//
-//    lazy var flagImageView: UIImageView = {
-//        let image = UIImageView()
-//        image.contentMode = .scaleAspectFit
-//        image.image = UIImage(named: "testImage")
-//        image.translatesAutoresizingMaskIntoConstraints = false
-//        return image
-//    }()
-//    
-//    lazy var nameLabel: UILabel = {
-//        let label = UILabel()
-//        label.textColor = .label
-//        label.font = UIFont(name: "SFPro-Regular", size: 14)
-//        label.textAlignment = .right
-//        label.text = "Georgia"
-//        label.translatesAutoresizingMaskIntoConstraints = false
-//        return label
-//    }()
-//    
-//    
-//    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
-//        super.init(style: style, reuseIdentifier: reuseIdentifier)
-//        self.setupUI()
-//    }
-//    
-//    required init?(coder: NSCoder) {
-//        fatalError("init(coder:) has not been implemented")
-//    }
-//    
-//    private func setupUI() {
-//        backgroundColor = .systemBackground
-//        clipsToBounds = true
-//        layer.borderWidth = 1
-//        layer.borderColor = UIColor.darkGray.cgColor
-//        layer.cornerRadius = 25
-//        accessoryType = .disclosureIndicator
-//        
-//        contentView.addSubview(flagImageView)
-//        contentView.addSubview(nameLabel)
-//        NSLayoutConstraint.activate([
-//            flagImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 15),
-//            flagImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 24),
-//            flagImageView.heightAnchor.constraint(equalToConstant: 20),
-//            flagImageView.widthAnchor.constraint(equalToConstant: 30),
-//            
-//            
-//            nameLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 15),
-//            nameLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -15),
-//            nameLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -16),
-//        ])
-//    }
-//    
-//    public func configure(image: String?, name: String?){
-//        nameLabel.text = name
-//        if let image = URL(string: image ?? "") {
-//            loadImage(url: image)
-//        }
-//        
-//    }
-//    func loadImage(url: URL) {
-//        
-//        URLSession.shared.dataTask(with: url) { [weak self] data, response, error in
-//            guard let data = data, let image = UIImage(data: data), error == nil else {
-//                print("Error loading image: \(error?.localizedDescription ?? "Unknown error")")
-//                return
-//            }
-//            DispatchQueue.main.async {
-//                self?.flagImageView.image = image
-//            }
-//        }.resume()
-//    }
-//  
-//}
-//
+
 import UIKit
  
 class CountriesTableViewCell: UITableViewCell {
     static let identifier = "CountriesTableViewCell"
-    
-//    var viewModel: CountryCellViewModel? {
-//        didSet {
-//            configure()
-//        }
-//    }
     
     // MARK: - UI Components
     lazy var rectangleView: UIView = {
